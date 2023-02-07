5a50460841d4e9e85b61d424e1027a048e2a94a8
Documenter.jl <documenter@juliadocs.github.io>
2023-02-07 14:14:20 +0000

    build based on febffdd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuliaProjectTemplate","category":"page"},{"location":"#JuliaProjectTemplate","page":"Home","title":"JuliaProjectTemplate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuliaProjectTemplate.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuliaProjectTemplate]","category":"page"}]
+}

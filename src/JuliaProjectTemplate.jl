3441c29ec0551fa84cb1b0557215e8eae06dec4f
javier <javier.gonzalez.monge@gmail.com>
2023-02-07 15:25:49 +0100

    Added basic functions

--- src/JuliaProjectTemplate.jl
+++ src/JuliaProjectTemplate.jl
@@ -1,5 +1,54 @@
 module JuliaProjectTemplate
 
-# Write your package code here.
+"""
+    add(x, y)
 
-end
+Computes the sum between `x` and `y`.
+"""
+add(x::T, y::T) where {T<:Number} = x + y
+add(x, y) = add(promote(x, y)...)
+
+"""
+    subtract(x, y)
+
+Computes the difference between `x` and `y`.
+"""
+subtract(x::T, y::T) where {T<:Number} = x - y
+subtract(x, y) = subtract(promote(x, y)...)
+
+"""
+    multiply(x, y)
+
+Computes the product between `x` and `y`.
+"""
+multiply(x::T, y::T) where {T<:Number} = x * y
+multiply(x, y) = multiply(promote(x, y)...)
+
+"""
+    divide(x, y)
+
+Computes the quotient between `x` and `y`.
+"""
+divide(x::T, y::T) where {T<:Number} = x / y
+divide(x, y) = divide(promote(x, y)...)
+
+"""
+    factorial(n)
+
+Computes the factorial of `n`.
+`` n! = n \\cdot (n - 1) \\cdot (n - 2) \\cdot ... \\cdot 1 ``
+"""
+myfactorial(n::T) where {T<:Integer} = n <= 1 ? 1 : n * factorial(n - 1)
+
+"""
+    fibonacci(n)
+
+Computes the `n`th Fibonacci number.
+`` F_n = F_{n - 1} + F_{n - 2} ``
+"""
+fibonacci(n::T) where {T<:Integer} = n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2)
+
+
+export add, subtract, multiply, divide, myfactorial, fibonacci
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
 using JuliaProjectTemplate
 using Test
 
-@testset "JuliaProjectTemplate.jl" begin
-    # Write your tests here.
+@testset "Basic operations" begin
+    @test add(1, 2) == 3
+    @test add(2.0, 7) == 9.0
+    @test subtract(1, 2) == -1
+    @test subtract(2.0, 7) == -5.0
+    @test multiply(1, 2) == 2
+    @test multiply(2.0, 7) == 14.0
+    @test divide(1, 2) == 0.5
+    @test divide(2.0, 7) ≈ 0.2857142857142857
+end
+
+@testset "Factorial" begin
+    @test factorial(0) == 1
+    @test factorial(1) == 1
+    @test factorial(2) == 2
+    @test factorial(3) == 6
+    @test factorial(4) == 24
+    @test factorial(5) == 120
+    @test factorial(6) == 720
+    @test factorial(7) == 5040
+    @test factorial(8) == 40320
+    @test factorial(9) == 362880
+    @test factorial(10) == 3628800
+end
+
+@testset "Fibonacci" begin
+    @test fibonacci(0) == 0
+    @test fibonacci(1) == 1
+    @test fibonacci(2) == 1
+    @test fibonacci(3) == 2
+    @test fibonacci(4) == 3
+    @test fibonacci(5) == 5
+    @test fibonacci(6) == 8
+    @test fibonacci(7) == 13
+    @test fibonacci(8) == 21
+    @test fibonacci(9) == 34
+    @test fibonacci(10) == 55
 end
